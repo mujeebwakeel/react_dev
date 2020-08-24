2603b19a22722008c77b3650d71b025554067a2a
mujeebwakeel <wakeelmujeeb@yahoo.com>
2020-08-24 21:35:28 +0100

    Terms and contact

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -122,16 +122,13 @@ function App(props) {
         		</aside>
 
 				<div  className="terms">
-					<div className="inner-terms">We use cookies to save your registered information for easy user navigation on this site. By continuing to browse this site you agree that any data submitted by you can be saved into our database,
-						and we shall not be liable to any network related isuues you encounter browing this site. You also agree
-						that any payment made on this site shall not be refunded. Thank you.
-					</div>
+					We use cookies to save your registered information for easy user navigation on this site. By continuing to browse this site you agree that any data submitted by you can be saved into our database,
+					and we shall not be liable to any network related isuues you encounter browing this site. You also agree
+					that any payment made on this site shall not be refunded. Thank you.
 				</div>
 
 				<div className="contact">
-						<div className="inner-contact">
 						  	You can reach us via <a href="mailto:wakeelmujeeb@yahoo.com?subject=Whykay%20Concept">wakeelmujeeb@yahoo.com</a>
-						</div>
 				</div>
 
         		<footer className="footer">
